27a9633363ba70c613877ce0cf08825f11642571
Sergio Pessegueiro <iFrancisco.sp@icloud.com>
2021-09-13 09:57:18 +0100

    First commit

---
+++ APIClientApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace APIClientApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
